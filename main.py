6db5384248ece68339dc060591a51f125700678d
RJomkar2002 <omkarramgirwar9@gmail.com>
2025-02-25 19:32:20 +0530

    updated

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ logging.basicConfig(filename="checklogs.txt", format='%(name)s : %(asctime)s : %
 logger=logging.getLogger("main.py")
 
 def main():
-    choice=int(input(" Add[1] \n Sub[2] \n enter choice for : "))
-    if choice==1:
+    choice=input(" Add[1] \n Sub[2] \n enter choice for : ")
+    if choice=="1":
             num1=input("enter 1st num : ")
             num2=input("enter 2nd num : ")
             add(num1,num2)
 
-    elif choice==2:
+    elif choice=="2":
             num1 = input("enter 1st num : ")
             num2 = input("enter 2nd num : ")
             sub(num1, num2)
 
     else:
-        logger.error("Invalid input enter valid choice")
+        logger.error("Invalid input enter valid choice : %s ",choice)
 
 
 if __name__=="__main__":
